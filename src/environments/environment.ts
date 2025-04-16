853b2c131d785051ee6f0476e411d36550a9a672
unknown <chipngaocho123@gmail.com>
2025-04-16 22:49:35 +0700

    edit & delete home page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const IP='127.0.0.1'
 export const environment = {
-    producttion:false,
+    producttion:true,
     apiGetUsers:`http://${IP}:8082/user`,
     apiGetAtmUser: `http://${IP}:8082/Atm/get`,
     apiGetFullUser: `http://${IP}:8082/admin/allUsers`,
